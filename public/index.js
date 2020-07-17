14810432a60a6c738727faf42b020df4a23e271a
Anthony Do <anthony4do@gmail.com>
2020-07-17 16:10:40 -0700

    Added local storage saving

--- public/index.js
+++ public/index.js
@@ -1,12 +1,22 @@
-const defaultHeader = `https://lybit.herokuapp.com/`;
-
+//const defaultHeader = `https://lybit.herokuapp.com/`;
+const defaultHeader = `http://localhost:3000/`;
 let localcache = [];
 
+window.onbeforeunload = closingCode;
 
 document.addEventListener('DOMContentLoaded', () => {
     const urlInput = document.getElementById('urlinput');
     const button = document.getElementById('submitbutton');
+    const clearButton = document.getElementById('clearbutton');
 
+    const json = JSON.parse(localStorage.getItem('LocalUrlCache'));
+
+    for(let i = json.length - 1; i >= 0; i--) {
+        let element = json[i];
+        console.log(element.title);
+        createCard(element.title, element.original, element.shortened);
+    }
+   
     button.onclick = () => {
         fetch(defaultHeader, {
             method: 'POST',
@@ -19,36 +29,40 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(response => {
             response.json().then( (data) => {
-                createCard(urlInput.value, defaultHeader + data.url);
+                createCard('Click to Edit Title', urlInput.value, defaultHeader + data.url);
             });
         });
     };
+
+    clearButton.onclick = () => {
+        localcache = [];
+        document.getElementById('list').innerHTML = '';
+    }
 })
 
 
-function addToLocalCache(original, shortened) {
-    localcache.push({
-        original: original,
-        shortened: shortened
-    })
-    console.log(localcache);
-}
+// function addToLocalCache(original, shortened) {
+//     localcache.push({
+//         original: original,
+//         shortened: shortened
+//     })
+//     console.log(localcache);
+//     closingCode();
+// }
 
 
-function createCard(original, shortened) {
+function createCard(title, original, shortened) {
     const list = document.getElementById('list');
     list.insertAdjacentHTML('afterbegin', 
         `<div class="card">
-            <div class="card-body">
-                <h4 class="card-title" contenteditable="true">Click to edit title</h4>
-                <p class="card-text">Original Link: ${original}</p>
-                <p class="card-text">Shortened Link: ${shortened}</p>
+            <div class="card-body" id="info">
+                <h4 class="card-title" id="title" contenteditable="true">${title}</h4>
+                <p class="card-text" id="original">Original Link: ${original}</p>
+                <p class="card-text" id="shortened">Shortened Link: ${shortened}</p>
                 <button type="button" onclick="copyText(this.id)" id="${shortened}" class="btn btn-primary">Copy Link</button>
             </div>
         </div>`
     );
-    addToLocalCache(original, shortened);
-    console.log(localcache);
 }
 
 
@@ -59,4 +73,19 @@ function copyText(text) {
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
+}
+
+
+function closingCode() {
+    localcache = [];
+    const elements = document.getElementById('list').children;
+    for (let element of elements) {
+        const info = element.querySelector('#info');
+        localcache.push({
+            title: info.querySelector('#title').innerHTML,
+            original: info.querySelector('#original').innerHTML.replace("Original Link: ", ""),
+            shortened: info.querySelector('#shortened').innerHTML.replace("Shortened Link: ", "")
+        })
+    }
+    localStorage.setItem('LocalUrlCache', JSON.stringify(localcache));
 }
